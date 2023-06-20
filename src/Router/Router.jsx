4174bf52d03241e962630151d13471999aac8d10
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-06-20 11:52:55 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainComponents from "../Layout/MainComponents";
+
+
+export const router = createBrowserRouter([
+{ path:'/',
+element:<MainComponents/>,
+ children:[
+    
+ ]
+
+}
+])
